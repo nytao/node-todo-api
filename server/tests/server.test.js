37c20828cb1405ddfe1461f9c2895e4b29ce152c
James <James@legend.local>
2018-08-08 21:19:50 -0400

    mocha.js expect.js supertest.js are added for testing

---
+++ server/tests/server.test.js
@@ -0,0 +1,58 @@
+const expect = require('expect');
+const supertest = require('supertest');
+
+const {app} = require('../server');
+const {Todo} = require('../models/todo');
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+  it('should create a new todo', (done) => {
+    var text = "This is a text.";
+    supertest(app)
+      .post('/todos')
+      .send({
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }, (err) => {
+          doen(err);
+        });
+      });
+  });
+
+  it('should not create a collection', (done) => {
+    var text = "ab";
+    supertest(app)
+      .post('/todos')
+      .send({text})
+      .expect(400)
+      .expect((res) => {
+        expect(res.body.text).toEqual(undefined);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.find().then((res) => {
+          expect(res.length).toBe(0);
+          done();
+        }, (err) => {
+          done(err);
+        });
+      })
+  });
+})
