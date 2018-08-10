8adc445124632d0c4e0097c56e1b0351f8114564
Tao <nytaoxu@gmail.com>
2018-08-10 18:10:46 -0400

    GET /todos added

--- server/server.js
+++ server/server.js
@@ -1,30 +1,46 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+const { mongoose } = require("./db/mongoose");
+const { Todo } = require("./models/todo");
+const { User } = require("./models/user");
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post("/todos", (req, res) => {
   console.log(req.body);
   var todo = new Todo({
     text: req.body.text
   });
-  todo.save().then((doc) => {
-    console.log(`Successfully saved! ${doc}`);
-    res.send(doc);
-  }, (err) => {
-    console.log(`Unable to save: ${err}`);
-    res.status(400).send(err);
-  });
+  todo.save().then(
+    doc => {
+      console.log(`Successfully saved! ${doc}`);
+      res.send(doc);
+    },
+    err => {
+      console.log(`Unable to save: ${err}`);
+      res.status(400).send(err);
+    }
+  );
+});
+
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    todos => {
+      console.log(`Successfully fetched! ${todos}`);
+      res.send({ todos });
+    },
+    err => {
+      console.log(`Unable to connect to the database. ${err}`);
+      res.status(400).send(err);
+    }
+  );
 });
 
 app.listen(8000, () => {
   console.log(`The server is listening on port 8000...`);
 });
 
-module.exports = {app};
+module.exports = { app };
