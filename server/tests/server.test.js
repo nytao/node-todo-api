f6f7e78a33fd79b6b3802419bccf5652343c5ef6
Tao <nytaoxu@gmail.com>
2018-08-31 21:29:42 -0400

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,17 @@
 const expect = require("expect");
 const supertest = require("supertest");
+const { ObjectId } = require("mongodb");
 
 const { app } = require("../server");
 const { Todo } = require("../models/todo");
 
 const todos = [
   {
+    _id: new ObjectId(),
     text: "first test todo"
   },
   {
+    _id: new ObjectId(),
     text: "second test todo"
   }
 ];
@@ -78,6 +81,43 @@ describe("GET /todos", () => {
       .expect(res => {
         expect(res.body.todos.length).toBe(2);
       })
+      .end(done);
+  });
+});
+
+describe("GET /todos/:id", () => {
+  it("should return a doc", done => {
+    supertest(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      // .expect((req, res) => {
+      //   expect(res.body.todos.text).toBe(todos[0].text);
+      // })
+      .expect(res => {
+        // console.log(res.body.todo._id.toString());
+        expect(res.body.todo.text).toEqual(todos[0].text);
+        expect(res.body.todo._id.toString()).toEqual(todos[0]._id.toString());
+      })
+      .end(done);
+  });
+
+  it("should return 404 if todo not found", done => {
+    let array = todos[0]._id.toString().split("");
+    array.splice(0, 2, "5", "7");
+    supertest(app)
+      .get(
+        // `/todos/${new ObjectId()}`
+        `/todos/${array.join("")}`
+        // `/todos/5789e75f28d57975ce9507d1`
+      )
+      .expect(404)
+      .end(done);
+  });
+
+  it("should return 400 if an invalid id is provided", done => {
+    supertest(app)
+      .get(`/todos/${new ObjectId()}7`)
+      .expect(400)
       .end(done);
   });
 });
